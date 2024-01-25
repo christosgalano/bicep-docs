c2a9a32e32911ca181a26b3871a7b3c4d9191d90
Christos Galanopoulos <christosgalanop@gmail.com>
2024-01-25 13:48:40 +0200

    Update content

--- internal/template/build.go
+++ internal/template/build.go
@@ -28,14 +28,43 @@ func BuildBicepTemplate(bicepFile string) (string, error) {
 	tmp := os.TempDir()
 	armFile := filepath.Join(tmp, fmt.Sprintf("%s_%s.json", filename, uuid.New().String()))
 
-	// Build Bicep template using azure-cli
+	// Build Bicep template into an ARM template
+	// 1. If 'bicep' exists, run 'bicep build'
+	// 2. If 'bicep' does not exist, check if 'az' exists
+	// 3. If 'az' exists, run 'az bicep build'
+	// 4. If 'az' does not exist, return an error
 	var cmd *exec.Cmd
-	cmd = exec.Command("az", "bicep", "build", "--file", bicepFile, "--outfile", armFile)
+	if commandExists("bicep") {
+		cmd = exec.Command("bicep", "build", bicepFile, "--outfile", armFile)
+	} else if commandExists("az") {
+		cmd = exec.Command("az", "bicep", "build", "--file", bicepFile, "--outfile", armFile)
+	} else {
+		return "", fmt.Errorf("neither 'bicep' nor 'az' commands were found")
+	}
+
+	// Run the command and handle any errors
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		return "", fmt.Errorf("failed to build Bicep template: %w", err)
+	if err := runCommand(cmd); err != nil {
+		return "", fmt.Errorf("failed to run command: %w", err)
 	}
 
 	return armFile, nil
+}
+
+// commandExists checks if a command exists.
+func commandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
+
+// runCommand runs a command and returns an error if it fails.
+func runCommand(cmd *exec.Cmd) error {
+	err := cmd.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return fmt.Errorf("%s: %s", err, exitError.Stderr)
+		}
+		return err
+	}
+	return nil
 }
